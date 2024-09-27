35a1649ad910f08b29a0f16a60d67930876b5e71
whitebearOuO <whitebearouob@gmail.com>
2024-09-27 11:41:58 +0800

    translate to en

--- week1/first.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    int iNum=0;
-    printf("enter a number:\n");
-    scanf("%d", &iNum);
-    printf("Number1: %d\n", iNum);
-}//end main
--- week1/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    char chose;
-    printf("請輸入你的選擇[1/2]: \n", scanf("%c", &chose));
-    printf("%c",&chose);
-}
